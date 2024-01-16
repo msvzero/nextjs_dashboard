3cfce8b4f9e281fff423b7054aad6fb7ecdf442f
msvzero2021 <msvzero2021@gmail.com>
2024-01-15 21:28:33 -0300

    Agregado fuente Lusitana

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,3 +1,5 @@
-import { League_Spartan } from 'next/font/google';
+import { League_Spartan, Lusitana } from 'next/font/google';
 
 export const spartan = League_Spartan({subsets: ['latin-ext'], weight: ['400', '700']});
+
+export const lusitana = Lusitana({subsets: ['latin'], weight: '400'});
